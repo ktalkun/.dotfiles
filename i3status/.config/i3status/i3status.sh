f2121ac444d12551a31843d94238dd80afd30527
Kirill Tolkun <kirill@acer.localhost>
2020-03-02 12:10:33 +0300

    Update timeout for update i3status. Add checking docker, snapd, netword-manager, wpa_supplicant, winbind services

--- i3status/.config/i3status/i3status.sh
+++ i3status/.config/i3status/i3status.sh
@@ -57,6 +57,108 @@ do
 #	swap_separator="\"separator\":false"
 #	sedline="s/\"${swap_before}\"/\"${swap_module_name}\",${swap_markup},${swap_full_text},${swap_separator}},{\"name\":\"${swap_before}\"/"
 #	line=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+
+#	__        _____ _   _ ____ ___ _   _ ____
+#	\ \      / /_ _| \ | | __ )_ _| \ | |  _ \
+#	 \ \ /\ / / | ||  \| |  _ \| ||  \| | | | |
+#	  \ V  V /  | || |\  | |_) | || |\  | |_| |
+#	   \_/\_/  |___|_| \_|____/___|_| \_|____/
+#	
+	winbind=`systemctl status winbind.service | grep "Active" | awk -F" " '{ print $2}'`
+	winbind_color="#dddddd"
+	if [ "$winbind" = "inactive" ] || [ "$winbind" = "deactivating" ]; then winbind_color="#f59335"; fi
+	winbind_module_name="winbind_status"
+	winbind_before="disk_info"
+	winbind_markup="\"markup\":\"pango\""
+	winbind_full_text="\"full_text\":\"<span background="\'$winbind_color\'"> W <\/span>\""
+	winbind_separator="\"separator\":false"
+	sedline="s/\"${winbind_before}\"/\"${winbind_module_name}\",${winbind_markup},${winbind_full_text},${winbind_separator}},{\"name\":\"${winbind_before}\"/"
+	line=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+
+
+
+#	__        ______   _
+#	\ \      / /  _ \ / \
+#	 \ \ /\ / /| |_) / _ \
+#	  \ V  V / |  __/ ___ \
+#	   \_/\_/  |_| /_/   \_\
+#	
+#	 ____  _   _ ____  ____  _     ___ ____    _    _   _ _____
+#	/ ___|| | | |  _ \|  _ \| |   |_ _/ ___|  / \  | \ | |_   _|
+#	\___ \| | | | |_) | |_) | |    | | |     / _ \ |  \| | | |
+#	 ___) | |_| |  __/|  __/| |___ | | |___ / ___ \| |\  | | |
+#	|____/ \___/|_|   |_|   |_____|___\____/_/   \_\_| \_| |_|
+#
+	wpa_supplicant=`systemctl status wpa_supplicant.service | grep "Active" | awk -F" " '{ print $2}'`
+	wpa_supplicant_color="#dddddd"
+	if [ "$wpa_supplicant" = "inactive" ] || [ "$wpa_supplicant" = "deactivating" ]; then wpa_supplicant_color="#f59335"; fi
+	wpa_supplicant_module_name="wpa_supplicant_status"
+	wpa_supplicant_before="disk_info"
+	wpa_supplicant_markup="\"markup\":\"pango\""
+	wpa_supplicant_full_text="\"full_text\":\"<span background="\'$wpa_supplicant_color\'"> W <\/span>\""
+	wpa_supplicant_separator="\"separator\":false"
+	sedline="s/\"${wpa_supplicant_before}\"/\"${wpa_supplicant_module_name}\",${wpa_supplicant_markup},${wpa_supplicant_full_text},${wpa_supplicant_separator}},{\"name\":\"${wpa_supplicant_before}\"/"
+	line=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+
+
+#	 _   _ _____ _______        _____  ____  _  __
+#	| \ | | ____|_   _\ \      / / _ \|  _ \| |/ /
+#	|  \| |  _|   | |  \ \ /\ / / | | | |_) | ' /
+#	| |\  | |___  | |   \ V  V /| |_| |  _ <| . \
+#	|_| \_|_____| |_|    \_/\_/  \___/|_| \_\_|\_\
+#	
+#	 __  __    _    _   _    _    ____ _____ ____
+#	|  \/  |  / \  | \ | |  / \  / ___| ____|  _ \
+#	| |\/| | / _ \ |  \| | / _ \| |  _|  _| | |_) |
+#	| |  | |/ ___ \| |\  |/ ___ \ |_| | |___|  _ <
+#	|_|  |_/_/   \_\_| \_/_/   \_\____|_____|_| \_\
+#	
+	network_manager=`systemctl status NetworkManager.service | grep "Active" | awk -F" " '{ print $2}'`
+	network_manager_color="#dddddd"
+	if [ "$network_manager" = "inactive" ] || [ "$network_manager" = "deactivating" ]; then network_manager_color="#f59335"; fi
+	network_manager_module_name="network_manager_status"
+	network_manager_before="disk_info"
+	network_manager_markup="\"markup\":\"pango\""
+	network_manager_full_text="\"full_text\":\"<span background="\'$network_manager_color\'"> N <\/span>\""
+	network_manager_separator="\"separator\":false"
+	sedline="s/\"${network_manager_before}\"/\"${network_manager_module_name}\",${network_manager_markup},${network_manager_full_text},${network_manager_separator}},{\"name\":\"${network_manager_before}\"/"
+	line=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+
+
+#	 ____  _   _    _    ____  ____
+#	/ ___|| \ | |  / \  |  _ \|  _ \
+#	\___ \|  \| | / _ \ | |_) | | | |
+#	 ___) | |\  |/ ___ \|  __/| |_| |
+#	|____/|_| \_/_/   \_\_|   |____/
+#	
+	snapd=`systemctl status snapd.service | grep "Active" | awk -F" " '{ print $2}'`
+	snapd_color="#dddddd"
+	if [ "$snapd" = "inactive" ] || [ "$snapd" = "deactivating" ]; then snapd_color="#f59335"; fi
+	snapd_module_name="snapd_status"
+	snapd_before="disk_info"
+	snapd_markup="\"markup\":\"pango\""
+	snapd_full_text="\"full_text\":\"<span background="\'$snapd_color\'"> S <\/span>\""
+	snapd_separator="\"separator\":false"
+	sedline="s/\"${snapd_before}\"/\"${snapd_module_name}\",${snapd_markup},${snapd_full_text},${snapd_separator}},{\"name\":\"${snapd_before}\"/"
+	line=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+
+#	 ____   ___   ____ _  _______ ____
+#	|  _ \ / _ \ / ___| |/ / ____|  _ \
+#	| | | | | | | |   | ' /|  _| | |_) |
+#	| |_| | |_| | |___| . \| |___|  _ <
+#	|____/ \___/ \____|_|\_\_____|_| \_\
+#	
+	docker=`systemctl status docker.service | grep "Active" | awk -F" " '{ print $2}'`
+	docker_color="#dddddd"
+	if [ "$docker" = "inactive" ] || [ "$docker" = "deactivating" ]; then docker_color="#f59335"; fi
+	docker_module_name="docker_status"
+	docker_before="disk_info"
+	docker_markup="\"markup\":\"pango\""
+	docker_full_text="\"full_text\":\"<span background="\'$docker_color\'"> D <\/span>\""
+	docker_separator="\"separator\":false"
+	sedline="s/\"${docker_before}\"/\"${docker_module_name}\",${docker_markup},${docker_full_text},${docker_separator}},{\"name\":\"${docker_before}\"/"
+	line=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+
 	
 #	 ____      _    __  __ 
 #	|  _ \    / \  |  \/  |
